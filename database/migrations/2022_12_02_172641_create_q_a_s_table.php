14d830913f7e07fbb8b5f8bebda3783280faf5e3
Abeer-Yahya <abeer.yahya799@gmail.com>
2022-12-03 13:05:55 +0300

    Initial commit

---
+++ database/migrations/2022_12_02_172641_create_q_a_s_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('q_a_s', function (Blueprint $table) {
+            $table->id();
+            $table->mediumText('question');
+            $table->longText('answer');
+            $table->string('category');
+            $table->boolean('closed');
+            $table->foreignId('user_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('q_a_s');
+    }
+};
